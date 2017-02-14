55b792e72d25266f10e5c1328649713542af029d
Darren <darrenenos@gmail.com>
2017-02-13 21:28:23 -0700

    fixed bootstrap and added template for front page

--- sdnSite/home/models.py
+++ sdnSite/home/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 class Posting(models.Model):
-    posting_text = models.CharField(max_length=200)
-    posting_type = models.CharField(max_length = 12)
-    posting_id = models.AutoField(primary_key=True)
+    #posting_text = models.CharField(max_length=200)
+    #posting_type = models.CharField(max_length = 12)
+    # posting_id = models.AutoField(primary_key=True)
     pub_date = models.DateTimeField('date published')

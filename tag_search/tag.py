47b44b27e6a3c3561134291b3fbca2ef4ae52249
Scott Franz <marshallwellington@gmail.com>
2017-02-16 10:22:55 -0700

    Began implementing tag search. Still figuring out how to use django.db.models

---
+++ tag_search/tag.py
@@ -0,0 +1,33 @@
+##Author: Scott Franz
+#Base tag class used to derive other tags from.
+
+
+class tag():
+    """
+    Base tag class for other tag types in tag_search.
+    Params:
+    self.id 'String id of tag.'
+    self.weight 'Float weight of object.'
+    self.link_array 'List of tags or objects this tag is
+    associated with.'
+    """
+    
+    def __init__(self, id, weight, link_array):
+        self.id = id
+        self.weight = weight
+        self.link_array = link_array
+    
+        
+    def to_string(self):
+        return "tag::tag(" + str(self.id) + "," + str(self.weight) + "," + str(self.link_array) + ")"
+        
+    def add_link(self, other):
+        """Add a reference to another tag or other object.
+        Params:
+        other 'Other object this tag should link to.'
+        """
+        
+        self.link_array.append(other)
+        
+    
+    
---
+++ tag_search/tag_search.py
@@ -0,0 +1,11 @@
+##Author: Scott Franz
+#Class to handle tag search in python.
+
+import tag
+
+class tag_search:
+
+    def __init__(self):
+        self.tag_array = []
+        
+    def search(self):

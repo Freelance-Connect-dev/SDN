ec6c761d96ab5b976739ef220f92fa50db6e2c68
aschurg <austin.schurg@gmail.com>
2017-03-30 15:33:32 -0600

    Added Contract and Application tables to DB
    also fixed some other tables and fields not needed anymore

--- sdnSite/home/models.py
+++ sdnSite/home/models.py
@@ -2,37 +2,32 @@ from django.db import models
 from django.utils import timezone
 
 # Create your models here.
-class Posting(models.Model):
-
-	job_id = models.IntegerField(default=0);
-	contractor_id = models.CharField(default=0, max_length=30)
-	employer_id = models.CharField(default=1, max_length=30)
-	job_title = models.CharField(default="make an app", max_length=30)
-	description = models.CharField(default="a decent job", max_length=500)
-	#status could be set up as a list of choices
-	finish_date = models.DateTimeField(default=timezone.now)
-	post_date = models.DateTimeField(default=timezone.now)
-	pay_amount = models.FloatField(default=1000.95)
 
 class Member(models.Model):
 	email = models.EmailField(primary_key=True)
-	name = models.CharField(max_length=50,unique=True)
+	name = models.CharField(max_length=50)
 	phone = models.IntegerField()
 	address = models.CharField(max_length=50)
 	biography = models.CharField(max_length=300)
 	business = models.BooleanField(default=False)
-	has_Website = models.BooleanField(default=False)
-	has_Mobile_App = models.BooleanField(default=False)
-	twitterAccount = models.CharField(max_length=50,default=None)
 	#Account links could become its own table to
 	#ensure that the data entered remains atomic.
+
+class Posting(models.Model):
+	job_id = models.IntegerField(default=0);
+	employer_id = models.ForeignKey(Member, default=0, on_delete=models.CASCADE)
+	job_title = models.CharField(default="make an app", max_length=50)
+	description = models.CharField(default="a decent job", max_length=1000)
+	#status could be set up as a list of choices
+	status = models.IntegerField(default=0)
+	finish_date = models.DateTimeField(default=timezone.now)
+	post_date = models.DateTimeField(default=timezone.now)
 
 class Image(models.Model):
 	image_id = models.CharField(max_length=30)
 	member_id = models.ForeignKey(Member,on_delete=models.CASCADE)
 	#image_reference = models.ImageField() #lets just fill this attribute with image links
 	title = models.CharField(max_length=30)
-	profile_picture = models.BooleanField(default=False);
 
 class File(models.Model):
 	file_id = models.IntegerField();
@@ -57,3 +52,20 @@ class Review(models.Model):
 	job_id = models.ForeignKey(Posting,on_delete=models.CASCADE)
 	reviewer_id = models.ForeignKey(Member,on_delete=models.CASCADE)
 	#reviewee_id = models.ForeignKey(Member,on_delete=models.CASCADE)
+
+class Contract(models.Model):
+	contract_id = models.IntegerField(primary_key=True)
+	job_id = models.ForeignKey(Posting,on_delete=models.CASCADE)
+	contractor_id = models.ForeignKey(Member, on_delete=models.CASCADE)
+	deliverable = models.CharField(max_length=1000)
+	total_pay = models.FloatField()
+	percent_up_front = models.FloatField()
+	create_date = models.DateTimeField(default=timezone.now)
+
+class Application(models.Model):
+	app_id = models.IntegerField(primary_key=True)
+	job_id = models.ForeignKey(Posting)
+	member_id = models.ForeignKey(Member)
+	create_date = models.DateTimeField(default=timezone.now)
+	cover_letter = models.CharField(max_length=1000)
+	resume_id = models.ForeignKey(File)
